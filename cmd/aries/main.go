e38230f9b6f1bbd56ba51ddaecc8c4f6df6e1d80
Lou Foster <lf6f@virginia.edu>
2018-10-17 14:03:35 -0400

    healthcheck pings service url without id to see if it is alive. resource requests handle services that become unavailable.

--- cmd/aries/main.go
+++ cmd/aries/main.go
@@ -23,6 +23,7 @@ const version = "1.0.0"
 type serviceInfo struct {
 	Name string
 	URL  string
+	OK   bool
 }
 
 // resoursesResponse is the respoonse format for a resources request
@@ -34,7 +35,7 @@ type resoursesResponse struct {
 }
 
 // services is a list of services known to Aries
-var services []serviceInfo
+var services []*serviceInfo
 
 // versionHandler reports the version of the serivce
 func versionHandler(c *gin.Context) {
@@ -43,7 +44,17 @@ func versionHandler(c *gin.Context) {
 
 // healthCheckHandler reports the health of the serivce
 func healthCheckHandler(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{"alive": "true"})
+	hcMap := make(map[string]string)
+	hcMap["alive"] = "true"
+	for _, svc := range services {
+		log.Printf("svs %s curr status %t", svc.Name, svc.OK)
+		if pingService(svc) {
+			hcMap[svc.Name] = "true"
+		} else {
+			hcMap[svc.Name] = "false"
+		}
+	}
+	c.JSON(http.StatusOK, hcMap)
 }
 
 // servicesHandler reports name and URL of all services seached by aries
@@ -59,14 +70,26 @@ func resourcesHandler(c *gin.Context) {
 	start := time.Now()
 
 	// kick off all requests in parallel
+	outstandingRequests := 0
 	for _, svc := range services {
-		url := fmt.Sprintf("%s/%s", svc.URL, id)
+		if svc.OK == false {
+			log.Printf("Service %s is currently not active. See if it is now available...", svc.Name)
+			if !pingService(svc) {
+				log.Printf("   ...Skipping")
+				out.Responses = append(out.Responses, gin.H{
+					"system": svc.Name, "status": http.StatusServiceUnavailable,
+					"response": "system is offline", "response_time_ms": 0,
+				})
+				continue
+			}
+		}
 		log.Printf("Check %s : %s for identifier %s", svc.Name, svc.URL, id)
-		go getAriesResponse(svc.Name, url, channel)
+		outstandingRequests++
+		go getAriesResponse(svc, id, channel)
 	}
 
 	// wait for all to be done and get respnses as they come in
-	for range services {
+	for outstandingRequests > 0 {
 		var jsonMap map[string]interface{}
 		jsonRespStr := <-channel
 		json.Unmarshal([]byte(jsonRespStr), &jsonMap)
@@ -75,6 +98,7 @@ func resourcesHandler(c *gin.Context) {
 			out.Hits++
 		}
 		out.Responses = append(out.Responses, jsonMap)
+		outstandingRequests--
 	}
 
 	elapsedNanoSec := time.Since(start)
@@ -83,11 +107,13 @@ func resourcesHandler(c *gin.Context) {
 	c.JSON(http.StatusOK, out)
 }
 
-func getAriesResponse(system string, url string, channel chan string) {
+func getAriesResponse(svc *serviceInfo, id string, channel chan string) {
+	url := fmt.Sprintf("%s/%s", svc.URL, id)
 	timeout := time.Duration(10 * time.Second)
 	client := http.Client{
 		Timeout: timeout,
 	}
+
 	start := time.Now()
 	resp, err := client.Get(url)
 	elapsedNanoSec := time.Since(start)
@@ -96,23 +122,31 @@ func getAriesResponse(system string, url string, channel chan string) {
 	if err != nil {
 		log.Printf("ERROR: GET %s failed : %s", url, err.Error())
 		status := http.StatusBadRequest
+		errMsg := err.Error()
 		if strings.Contains(err.Error(), "Timeout") {
 			status = http.StatusRequestTimeout
+			errMsg = "request timed out"
+		} else if strings.Contains(err.Error(), "connection refused") {
+			status = http.StatusServiceUnavailable
+			errMsg = "system is offline"
 		}
+		svc.OK = false
 		channel <- fmt.Sprintf(`{"system": "%s", "status": %d, "response": "%s", "response_time_ms": %d}`,
-			system, status, err.Error(), elapsedMS)
+			svc.Name, status, errMsg, elapsedMS)
 		return
 	}
+
 	defer resp.Body.Close()
 	bodyBytes, _ := ioutil.ReadAll(resp.Body)
 	respString := string(bodyBytes)
 	if resp.StatusCode != 200 {
 		channel <- fmt.Sprintf(`{"system": "%s", "status": %d, "response": "%s", "response_time_ms": %d}`,
-			system, resp.StatusCode, respString, elapsedMS)
+			svc.Name, resp.StatusCode, respString, elapsedMS)
 		return
 	}
+
 	channel <- fmt.Sprintf(`{"system": "%s", "status": %d, "response": %s, "response_time_ms": %d}`,
-		system, resp.StatusCode, respString, elapsedMS)
+		svc.Name, resp.StatusCode, respString, elapsedMS)
 }
 
 func initServices() error {
@@ -126,10 +160,43 @@ func initServices() error {
 		line := scanner.Text()
 		log.Printf("Got service: %s", line)
 		bits := strings.Split(line, ",")
-		services = append(services, serviceInfo{Name: bits[0], URL: bits[1]})
+
+		// create a and track a service; assume it is not alive by default
+		// ping  will test and update this alive status
+		svc := &serviceInfo{Name: bits[0], URL: bits[1], OK: false}
+		services = append(services, svc)
+		if !pingService(svc) {
+			log.Printf("ERROR: Service %s is not available", svc.Name)
+		}
 	}
 
 	return nil
+}
+
+// pingService will ping the service URL and return true if the serice responds
+// The status of the service object will be updated based on this test
+func pingService(svc *serviceInfo) bool {
+	timeout := time.Duration(2 * time.Second)
+	client := http.Client{
+		Timeout: timeout,
+	}
+
+	// Flag service as dead. If the test below is OK, update to alive
+	svc.OK = false
+	log.Printf("Ping sevice %s - %s...", svc.Name, svc.URL)
+	resp, err := client.Get(svc.URL)
+	if err != nil {
+		log.Printf("Ping FAILED : %s", err.Error())
+		return false
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != 200 {
+		log.Printf("Ping returned bad status code : %d: ", resp.StatusCode)
+		return false
+	}
+	log.Printf("%s is alive", svc.Name)
+	svc.OK = true
+	return true
 }
 
 /**
