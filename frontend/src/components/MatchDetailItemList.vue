739fc701276fa61821a26bd67d71284360114d92
Lou Foster <lf6f@virginia.edu>
2018-10-17 10:26:27 -0400

    show url type/format in service and metadata lists

--- frontend/src/components/MatchDetailItemList.vue
+++ frontend/src/components/MatchDetailItemList.vue
@@ -2,10 +2,12 @@
   <span v-if="items">
     <ol v-if="items.length > 1">
       <li v-for="(item, index) in items" :key="index">
+        <span class="url-type">{{ getItemType(item).value }} :</span>
         <a :href="item.url" target="_blank">{{ item.url }}</a>
       </li>
     </ol>
     <template v-else>
+      <span class="url-type">{{ getItemType(items[0]).value }} :</span>
       <a :href="items[0].url" target="_blank">{{ items[0].url }}</a>
     </template>
   </span>
@@ -19,11 +21,25 @@
         type: Array,
         required: true
       }
+    },
+    methods: {
+      getItemType: function(item) {
+        console.log("Get type for "+item)
+        for (var key in item) {
+          if (key !== "url") {
+            return {name: key, value: item[key]}
+          }
+        }
+      }
     }
   }
 </script>
 
 <style scoped>
+  span.url-type {
+    margin-right: 10px;
+    color: #999;
+  }
   ol {
     margin: 0;
     padding-inline-start: 20px;
